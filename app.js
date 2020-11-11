5ba142bba6f2a8ff5c182ab3784e06e2f69593c3
JoelSilvi <analistasilvi@gmail.com>
2020-11-11 00:25:04 -0300

    if- else if - else - prompt - alert -operador comparacion

---
+++ app.js
@@ -0,0 +1,68 @@
+let dinero_joel = prompt ("cuanto dinero tiene joel?");
+let dinero_jorge = prompt ("cuanto dinero tiene jorge?");
+let dinero_misael = prompt ("cuanto dinero tiene misael?");
+
+if (dinero_joel >= 1 && dinero_joel <2){
+    alert ("joel te alcanza un helado de entre 1 y 1.9 dolares");
+
+}else if(dinero_joel >=2 && dinero_joel <3){
+    alert ("joel te alcanza un helado de entre 2 y 2.9 dolares");
+
+}else if(dinero_joel>=3 && dinero_joel<4){
+    alert ("joel te alcanza un helado entre 3 y 3.9 dolares");
+
+}else if(dinero_joel>=4 && dinero_joel<5){
+    alert ("joel te alcanza un helado entre 4 y 4,9 dolares");
+
+}else if(dinero_joel>=5){
+    alert ("joel te alcanza el que quieras");
+
+}else{
+    alert("joel no te alcanza para ninguno");
+}
+
+
+
+
+if (dinero_jorge >= 1 && dinero_jorge <2){
+    alert ("jorge te alcanza un helado de entre 1 y 1.9 dolares");
+
+}else if(dinero_jorge >=2 && dinero_jorge <3){
+    alert ("jorge te alcanza un helado de entre 2 y 2.9 dolares");
+
+}else if(dinero_jorge>=3 && dinero_jorge<4){
+    alert ("jorge te alcanza un helado entre 3 y 3.9 dolares");
+
+}else if(dinero_jorge>=4 && dinero_jorge<5){
+    alert ("jorge te alcanza un helado entre 4 y 4,9 dolares");
+
+}else if(dinero_jorge>=5){
+    alert ("jorge te alcanza el que quieras");
+
+}else{
+    alert("jorge no te alcanza para ninguno");
+}
+
+
+
+
+if (dinero_misael >= 1 && dinero_misael <2){
+    alert ("misael te alcanza un helado de entre 1 y 1.9 dolares");
+
+}else if(dinero_misael >=2 && dinero_misael <3){
+    alert ("misael te alcanza un helado de entre 2 y 2.9 dolares");
+
+}else if(dinero_misael>=3 && dinero_misael<4){
+    alert ("misael te alcanza un helado entre 3 y 3.9 dolares");
+
+}else if(dinero_misael>=4 && dinero_misael<5){
+    alert ("misael te alcanza un helado entre 4 y 4,9 dolares");
+
+}else if(dinero_misael>=5){
+    alert ("misael te alcanza el que quieras");
+
+}else{
+    alert("misael no te alcanza para ninguno");
+}
+
+
