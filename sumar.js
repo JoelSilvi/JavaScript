05abde8fb408e3bbd05ee65b21aaa3b6ebbd4fe5
JoelSilvi <analistasilvi@gmail.com>
2020-11-13 00:35:09 -0300

    funcion sumar-restar-saludo-valores

---
+++ sumar.js
@@ -0,0 +1,50 @@
+// function saludo() {
+//     respuesta = prompt("Hola cómo estás");
+//     if (respuesta=="bien"){
+//         alert("Super");
+//     }else{
+//         alert("Chuta");
+//     }
+// }
+
+// saludo();
+
+
+// function sumar() {
+//     let numero_0= 1;
+//     let numero_1= 5;
+//     suma = (numero_0 + numero_1);
+//     alert ("la suma es: " + suma)
+// }
+// sumar();
+
+
+// function restar(){
+//     let numero_2 = prompt ("Ingresa el primer numero");
+//     let numero_3 = prompt ("Ingresa el segundo número");
+//     resta = (numero_2 - numero_3);
+//     alert("la resta es: " + resta);
+// }
+
+// restar();
+
+
+// function saludar(nombre,edad){
+//     this.nombre=nombre;
+//     this.edad=edad;
+//     alert("tu nombre es: " + this.nombre + " y tu edad es: " + this.edad + " años");
+// }
+
+// saludar("joel",32);
+
+
+function valores(a,b,c){
+    return a+b+c;
+}
+
+let suma = valores(3,5,2);
+
+document.write(suma);
+
+console.log(suma);
+
