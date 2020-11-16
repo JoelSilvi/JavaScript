4208f94637a39dd00a16f8519ff6602369783916
JoelSilvi <analistasilvi@gmail.com>
2020-11-15 23:54:37 -0300

    variables y operadores

---
+++ operadores.js
@@ -0,0 +1,27 @@
+let number0 = 100;
+let number1 = 200;
+
+let resultado = number0 + number1;
+document.write(resultado + "<br>");
+
+
+let nombre = 'Joel';
+let apellido = 'Aguilar';
+
+let nombreCompleto = nombre + " " +  apellido;
+document.write(nombreCompleto + "<br>");
+
+
+let numberOne = 100;
+let numberTwo = 200;
+
+let result = numberOne > numberTwo;
+document.write(result);
+
+
+let passwordDB = 'misa123';
+let inputPasswordDB = 'misa12';
+
+let resultadoComparacion = passwordDB == inputPasswordDB;
+
+console.log(resultadoComparacion);
---
+++ variables.js
@@ -0,0 +1,26 @@
+//tipos de datos
+
+// console.log({"nombre":"joel", "apellido":"aguilar","edad":30});
+
+// nombreUsuario = "jhon";
+// console.log(nombreUsuario);
+
+// var nombre = "joel<br>";
+// let apellido = "juan";
+
+// nombre = 'pepe<br>';
+
+// document.write(nombre);
+// document.write(apellido);
+
+
+
+const PI = 3.1415;
+const ID = '0'
+
+console.log(PI);
+document.write(PI);
+
+
+// camelcase
+let nombreDePersona ='misa'; 
