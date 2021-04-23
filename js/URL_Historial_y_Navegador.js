87fa316f23438910c81048b1c6e1ca0e00b6186e
JoelSilvi <analistasilvi@gmail.com>
2021-04-23 02:49:56 -0400

    Objetos: URL, Historial y Navegador

---
+++ js/URL_Historial_y_Navegador.js
@@ -0,0 +1,35 @@
+// console.log("************** Objeto URL (location) ************")
+// console.log(location);
+// console.log(location.origin);
+// console.log(location.protocol);
+// console.log(location.host);
+// console.log(location.hostname);
+// console.log(location.port);
+// console.log(location.href);
+// console.log(location.hash);
+// console.log(location.search);
+// console.log(location.pathname);
+// setTimeout(() => {
+//     location.reload()    
+// }, 5000);
+
+
+// console.log("********* Objeto Historial **********");
+// console.log(history);
+// console.log(history.length);
+// history.back(2);
+// history.forward(1);
+// history.go(-1);
+// history.go(2);
+
+
+console.log("************ Objeto Navigator ***********");
+console.log(navigator.connection);
+console.log(navigator.geolocation);
+console.log(navigator.mediaDevices);
+console.log(navigator.mimeTypes);
+console.log(navigator.onLine);
+console.log(navigator.serviceWorker);
+console.log(navigator.storage);
+console.log(navigator.usb);
+console.log(navigator.userAgent);
