5945b9cceb20451ae895e6171fcd4fa34649763b
JoelSilvi <analistasilvi@gmail.com>
2021-05-03 21:22:08 -0400

    Envío Formulario y CORS con Fetch y PHP

---
+++ assets/send_mail.php
@@ -0,0 +1,46 @@
+<?php
+if(isset($_POST)){
+    error_reporting(0);
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $comments = $_POST["comments"];
+
+
+    $domain = $_SERVER["HTTP_HOST"];
+    $to = "analistasilvi@gmail.com";
+    $subject_mail = "Contacto desde el formulario del sitio $domain.";
+    $message = "<p> Datos enviados desde el sitio <b>$domain</b></p>
+    <ul>
+        <li>Nombre: <b>$name</b></li>
+        <li>Email: <b>$email</b></li>
+        <li>Asunto: <b>$subject</b></li>
+        <li>Comentarios: $comments </li>
+    </ul>
+    ";
+
+    $headers = "MIME-Version: 1.0\r\n" . "Content-Type: text/html; charset=utf-8\r\n" .
+    "From: Envío automático No responder <no-reply@$domain>";
+
+    $send_mail = mail($to, $subject_mail, $message, $headers);
+
+    if($send_mail){
+        $res = [
+            "err" => false,
+            "message" => "Tus datos han sido enviados"
+        ];
+    }else{
+        $res = [
+        "err" => true,
+        "message" => "Error al enviar tus datos. Intenta Nuevamente"
+        ];
+    }
+
+    header("Access-Control-Allow-Origin: *");
+    // CAMBIAR
+    // header("Access-Control-Allow-Origin: https://google.com");
+    // CAMBIAR
+    header('Content-type: application/json');
+    echo json_encode ($res);
+    exit;
+}
