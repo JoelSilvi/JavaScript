7c5129a838a943966642f1fcdfd0f7df0d155365
JoelSilvi <analistasilvi@gmail.com>
2021-05-25 20:43:23 -0400

    Refactorización de la App. Enrutamiento Asíncrono

--- app/components/Router.js
+++ app/components/Router.js
@@ -2,21 +2,21 @@ import api from "../helpers/wp_api.js";
 import {ajax} from "../helpers/ajax.js"
 import { PostCard } from "./PostCard.js";
 
-export function Router(){
+export async function Router(){
     const d = document,
         w = window,
-        $posts = d.getElementById("posts");
+        $main = d.getElementById("main");
 
         let{hash} = location;
 
         console.log(hash);
 
-        $posts.innerHTML = null;
+        $main.innerHTML = null;
 
 
 
         if(!hash || hash === "#/"){
-            ajax({
+            await ajax({
                 url: api.POSTS,
                 cbSuccess:(posts)=>{
                     // console.log(posts);
@@ -24,18 +24,16 @@ export function Router(){
                     
                         
                     posts.forEach((post) => (html += PostCard(post)));
-                    d.querySelector(".loader").style.display = "none";
-                    $posts.innerHTML = html;
+                    $main.innerHTML = html;
                 },
             });
         }else if(hash.includes("#/search")){
-            $posts.innerHTML = "<h2>Sección del Buscador</h2>";
+            $main.innerHTML = "<h2>Sección del Buscador</h2>";
         }else if(hash === "#/contacto"){
-            $posts.innerHTML = "<h2>Sección de Contacto</h2>";
+            $main.innerHTML = "<h2>Sección de Contacto</h2>";
         }else{
-            $posts.innerHTML = "<h2>Aquí cargará el contenido del el Post previamente Seleccionado</h2>";
+            $main.innerHTML = "<h2>Aquí cargará el contenido del el Post previamente Seleccionado</h2>";
         }
-
         
-
+        d.querySelector(".loader").style.display = "none";
 }
