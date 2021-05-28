7f66fb75bca8bc296044008a2c6057e16008ab15
JoelSilvi <analistasilvi@gmail.com>
2021-05-27 23:15:26 -0400

    Infinite Scroll & High Order Components

--- app/App.js
+++ app/App.js
@@ -2,14 +2,17 @@ import { Header } from "./components/Header.js";
 import { Main } from "./components/Main.js";
 import { Loader } from "./components/Loader.js";
 import { Router } from "./components/Router.js";
-    
-export function App(){
-    const $root = document.getElementById("root");
+import { InfiniteScroll } from "./helpers/infinite_scroll.js";
 
-        $root.innerHTML = null;
-        $root.appendChild(Header());
-        $root.appendChild(Main());
-        $root.appendChild(Loader());
-        Router();
+export function App() {
+  const $root = document.getElementById("root");
+
+  $root.innerHTML = null;
+  $root.appendChild(Header());
+  $root.appendChild(Main());
+  $root.appendChild(Loader());
+
+  Router();
+
+  InfiniteScroll();
 }
-
